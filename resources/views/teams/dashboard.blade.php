0baf0c4520dbe5b387a0016167ba3611c9af8b0d
fannyyrr29 <s160422005@student.ubaya.ac.id>
2023-06-04 00:39:00 +0700

    update dashboard lagii

--- resources/views/teams/dashboard.blade.php
+++ resources/views/teams/dashboard.blade.php
@@ -11,16 +11,73 @@
 @section('content')
 
 <style>
+    #cloud{
+        width: 10rem;
+        position: absolute;
+        bottom: -10rem;
+        left: 3rem;
+        transform: scaleX(-1);
+        z-index: -1;
+    }
     .cloud-dashboard{
         width: 350px;
         position: absolute;
+        left: -14rem;
+        top: -6rem;
+        transform: scaleX(-1);
+        filter: drop-shadow(0 0 5px #a36948);
+        z-index: -1;
+        
+    }
+    .moon{
+        position: absolute; 
+        bottom: 5rem; 
+        right: -2rem; 
+        width: 5rem;
+        z-index: -2;
+    }
+    .star1{
+        position: absolute;
+        right: 3rem;
+        top: 6.25rem;
+    }
+    .star2{
+        position: absolute;
+        left: 7rem;
+        top: 31.25rem;
+    }
+    .star3{
+        position: absolute;
+        left: 30rem;
+        bottom: -20rem;
+    }
+    .star{
+        animation: starAnimation 5s ease-in-out 1s infinite;
+    }
+    #fairytale{
+        width: 2rem;
+        position: absolute; 
+        top: 25rem;
+        right: 5rem;
+    }
+    @keyframes starAnimation {
+        0%,100%{
+            filter: drop-shadow(0 0 8px #ffd93d);
+        }
+        50%{
+            filter: drop-shadow(0 0 1px #ffd93d);
+        }
     }
 </style>
 
     <section id="dashboard" class="overflow-hidden">
+        <img src="{{ asset('../mainweb/img/maniacxii-asset/Asset 5.png') }}" alt="" class="star1 star">
+        <img src="{{ asset('../mainweb/img/maniacxii-asset/Asset 5.png') }}" alt="" class="star2 star">
+        <img src="{{ asset('../mainweb/img/maniacxii-asset/Asset 5.png') }}" alt="" class="star3 star">
+        <img src="{{ asset('../mainweb/img/maniacxii-asset/Asset 4.png') }}" alt="" id="fairytale">
     <div style="width: 90%; margin: 0 auto;">
     <div class="position-relative">
-        <img src="{{ asset('../mainweb/img/maniacxii-asset/Asset 15.png') }}" alt="" class="cloud-dashboard" style="margin-top: -55px; margin-left: -50px;">
+        <img src="{{ asset('../mainweb/img/maniacxii-asset/Asset 12.png') }}" alt="" class="cloud-dashboard">
         <div class="container dashboard-container">
             <div class="row">
                 <h3 class="mt-5"><strong>Selamat Datang, Tim {{ $team->team_name }}</strong></h3>
@@ -95,9 +152,10 @@
                 </div>
             </form>
         </div>
-        <img src="{{ asset('../mainweb/img/maniacxii-asset/Asset 15.png') }}" class="cloud-dashboard" style="right: 0; bottom: -50px;" alt="">
+        <img src="{{ asset('../mainweb/img/maniacxii-asset/Asset 3.png') }}" alt="" class="moon">
     </div>
     </div>
+    <img src="{{ asset('../mainweb/img/maniacxii-asset/Asset 11.png') }}" alt="" id="cloud">
     <img src="{{ asset('../mainweb/img/maniacxii-asset/Asset 10.png') }}" alt="" style="display: block; margin-left: auto; margin-right: -200px">
     </section>
 
